4590fa3653c1873bcb7170e86c98e139d5640fba
renji18 <aadarshjha1401@gmail.com>
2025-05-04 15:39:57 +0530

    chore: UI correction for access in small screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SQLConsole from "./components/SQLConsole.tsx";
 export default function App() {
 
   return (
-    <main className="min-h-screen bg-gray-100 flex items-center justify-start gap-8 py-12 px-4">
+    <main className="min-h-screen bg-gray-100 flex flex-wrap items-center justify-start gap-8 py-12 px-4">
       <PatientForm/>
       <SQLConsole/>
     </main>
